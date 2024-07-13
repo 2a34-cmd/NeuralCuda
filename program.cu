8fffc72d46f409cf7fb61befd3960b8ba4cdfc1c
2a34-cmd <khtably55@gmail.com>
2024-07-14 02:34:30 +0300

    final touches for cli usage

--- program.cu
+++ program.cu
@@ -14,28 +14,27 @@
 // 3- mnist label file path
 // 4- the index of first image to be trained
 // 5- learning rate
-
+// 6- number of parallel blocks where each one handle one of input/expected output training 
 
 int main(int argc,char *argv[]){
     //int GPUId = cudaGetDevice(&GPUId);
     printf("%d is num of args\n",argc);
-    // if(argc != 6){
-    //     printf("there must be 5 arguments\n");
-    //     return 5;
-    // }
-
+    if(argc != 7){
+        printf("there must be 6 arguments\n");
+        return -1;
+    }
+    int Stp = stoi(argv[4]),mlR = stod(argv[5]), pN = stoi(argv[6]);
     // unifed (accessible from both CPU & GPU) pointer to neural network
     neuralnetwork* NNp;
     cudaError_t err = cudaMallocManaged((void**)&NNp,sizeof(neuralnetwork));
     if(err != cudaSuccess){
-        cout << "there is problem in allocating memory for pointer\n";
+        printf("there is problem in allocating memory for pointer\n");
         cout << to_string(err);
-        return 3;
+        return -2;
     }
     //FromFile functoin will assign NNp with adress of neural network
     //since it's unifed memory (at least, in abstract manner), we can use GPU right away
-    neuralnetwork nn = FromFile("version10.mn1");
-    //NNp = &nn;
+    neuralnetwork nn = FromFile(argv[1]);
     cudaMemcpy(NNp,&nn,sizeof(nn),cudaMemcpyDefault);
     //cudaMemPrefetchAsync(NNp,sizeof(neuralnetwork),GPUId);
     
@@ -46,35 +45,21 @@ int main(int argc,char *argv[]){
 
     // getting both inputs(images) & outputs(labels) for neural network
     unsigned char** ITNN;
-    ITNN = InputsToNN(ITNN,"img.idx3",0);
+    ITNN = InputsToNN(ITNN,argv[2],Stp);
     unsigned char** EFNN;
     string v = "";
-    EFNN = ExpectedFromNN(EFNN,"lbl.idx1",0);
+    EFNN = ExpectedFromNN(EFNN,argv[3],Stp);
     // reapeting from starting point until reaching number of training epsidoes (which is 60,000)
-    for (int i = 0; i < 10000; i++)
+    for (int i = Stp; i < 10000; i+=pN)
     {
-        InputFirst<<<28,28>>>(NNp, ITNN[i]);
+        PreBackPropagation(NNp,&ITNN[i],&EFNN[i],mlR,pN);
         cudaDeviceSynchronize();
-        for (size_t j = 1; j < NNp->NumOfLayers; j++)
-        {
-            calc<<<8,32>>>(NNp, j);
-            cudaDeviceSynchronize();
-        }
-        printf("iteration %d : error %f\n",i,error(NNp,EFNN[i]));
+        printf("iteration %d : error %lf\n",i,error(NNp,EFNN[i]));
         cudaDeviceSynchronize();
-        diffLast<<<1,10>>>(NNp, (unsigned char *)EFNN[i], 2.0);
-        cudaDeviceSynchronize();
-        for (size_t j = NNp->NumOfLayers -2; j >= 1; j--)
-        {
-            diffcalc<<<25,32>>>(NNp, j);
-            cudaDeviceSynchronize();
-        }
-        // preback<<<1,784>>>(NNp,ITNN[i],EFNN[i],0.5);
-        // cin >> v;
         back<<<284,800>>>(NNp);
         cudaDeviceSynchronize();
     }
-    
+    ToFile("version11.mn1",NNp);
 
     cudaFree(NNp);
     cudaFree(ITNN);
